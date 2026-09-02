a17354b5ba1456d6647b882aa73d56681c921834
simo <138319362+spaysi69@users.noreply.github.com>
2026-09-02 16:13:48 +0100

    Delete momdiproject directory

--- momdiproject/scripts-copy-public.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require('node:fs');
-const path = require('node:path');
-const source = path.join(process.cwd(), 'src', 'server', 'public');
-const destination = path.join(process.cwd(), 'dist', 'src', 'server', 'public');
-fs.cpSync(source, destination, { recursive: true });
